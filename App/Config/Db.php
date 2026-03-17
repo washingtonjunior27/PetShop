f10cf450403787aa94dbea6030d8742f0e6258d8
Washington Junior <washjunior4444@gmail.com>
2026-03-17 18:28:07 -0400

    update conection and namespaces/models

---
+++ App/Config/Db.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Config;
+
+use PDO;
+use PDOException;
+
+class Connection
+{
+    private $db_name = "petshop";
+    private $db_host = "localhost";
+    private $db_user = "root";
+    private $db_password = "";
+
+    public function getConn()
+    {
+        try {
+            $con = new PDO("mysql:dbhost=" . $this->db_host . ",dbname=" . $this->db_name .
+                ",charset=utf8", $this->db_user, $this->db_password);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $con;
+        } catch (PDOException $e) {
+            echo "Erro de conexão" . $e->getMessage();
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+define("BASE_URL", "/Petshop");
+
+$route = $_GET['route'] ?? "Login";
